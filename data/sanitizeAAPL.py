bc1c4b65c039f5f383d8b43076e155020cbc99b4
daviddinh1 <91696365+daviddinh1@users.noreply.github.com>
2025-04-16 18:14:32 -0500

    Scaled data to work with MLP model

--- data/sanitizeAAPL.py
+++ data/sanitizeAAPL.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from sklearn.preprocessing import StandardScaler
+
 
 #import AAPL csv, csv data is different from yahoo finance
 csv_AAPL = pd.read_csv('individual_tickers/AAPL_historic.csv')
@@ -12,4 +14,16 @@ csv_AAPL['Date'] = pd.to_datetime(csv_AAPL['Date'])
 print(csv_AAPL.info())
 print(csv_AAPL.dtypes)
 
-#scale the data step 5 chatgpt then save or use clean data
+#After looking at data since volume is such a big number you need to scale out so model doesnt struggle
+#print(csv_AAPL.describe().to_string())
+
+#declare the features
+features = ['Close','High','Low','Open','Volume']
+
+#declare the type of scalar you're using
+scaler = StandardScaler()
+csv_AAPL[features] = scaler.fit_transform(csv_AAPL[features])
+
+print(csv_AAPL.describe().to_string())
+
+
