60cb96c932a86be9afdc453f92c60aa9d291404b
daviddinh1 <91696365+daviddinh1@users.noreply.github.com>
2025-04-16 18:28:01 -0500

    dropped date column for model and created new csv for data

--- data/sanitizeAAPL.py
+++ data/sanitizeAAPL.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
+
 
 
 #import AAPL csv, csv data is different from yahoo finance
@@ -25,5 +27,24 @@ scaler = StandardScaler()
 csv_AAPL[features] = scaler.fit_transform(csv_AAPL[features])
 
 print(csv_AAPL.describe().to_string())
+
+#this shows my plot as a symmetric plot
+# for feat in features:
+#     plt.figure()                # start a new figure for each feature
+#     csv_AAPL[feat].hist(bins=30)      # you can tweak the number of bins
+#     plt.title(f'Distribution of {feat}')
+#     plt.xlabel(feat)
+#     plt.ylabel('Frequency')
+#     plt.show()
+
+
+#drop unneeded values and then create a new csv
+# drops Date (or any other non‑numeric columns) if you haven’t already
+df_model = csv_AAPL.drop(columns=['Date'])
+
+# save
+df_model.to_csv('individual_tickers/scaled_data.csv', index=False)
+print("Saved cleaned & scaled data to scaled_data.csv")
+
 
 
