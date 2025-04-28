156d51c41f7d7ced7e7369110da923c993dcae52
daviddinh1 <91696365+daviddinh1@users.noreply.github.com>
2025-04-28 18:10:18 -0500

    updated model with more depth and width to improve r^2 and mse with yfinance data group is using

--- data/davidMlpModel.py
+++ data/davidMlpModel.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import joblib
+import yfinance as yf
 
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.neural_network import MLPRegressor
@@ -8,7 +9,12 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
 #load data here/use yahoo api figure this out
-csv_AAPL = pd.read_csv('individual_tickers/AAPL_historic.csv') #if you want latest date change to yahoo csv
+#yahoo data group is using
+ticker = "AAPL"
+start  = "2017-01-01"
+end    = "2025-01-01"
+csv_AAPL = yf.download(ticker, start=start, end=end).reset_index()
+# csv_AAPL = pd.read_csv('individual_tickers/AAPL_historic.csv') #if you want latest date change to yahoo csv
 csv_AAPL['Date'] = pd.to_datetime(csv_AAPL['Date'])
 features = ['Close','High','Low','Open','Volume']
 scaler = StandardScaler()
@@ -26,14 +32,26 @@ df_fe.dropna(inplace=True)
 
 #training and testing split
 X = df_fe.drop(columns=['Date','Close'])
-y = df_fe['Close']
+y = df_fe['Close'].to_numpy().ravel()
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, shuffle=False, random_state=42
 )
 
-#trained mlp model
+#trained mlp model with 2018-2023 data
+# mlp_model = MLPRegressor(
+#     hidden_layer_sizes=(128,128),
+#     alpha=1e-4,
+#     activation='relu',
+#     solver='adam',
+#     learning_rate_init=0.01,
+#     max_iter=200,
+#     random_state=42,
+#     verbose=True
+# )
+
+#using updated data
 mlp_model = MLPRegressor(
-    hidden_layer_sizes=(128,128),
+    hidden_layer_sizes=(128,128,128), #the more data there is the more neurons needed, due to our features
     alpha=1e-4,
     activation='relu',
     solver='adam',
